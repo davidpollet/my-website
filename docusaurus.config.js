25976139109f01bb3b3761a56184135b8bf7dbcc
David Pollet <aubonpixel@gmail.com>
2024-03-23 20:18:27 +0100

    init tailwind

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -34,6 +34,20 @@ const config = {
     locales: ["fr"],
   },
 
+  plugins: [
+    async function myPlugin(context, options) {
+      return {
+        name: "docusaurus-tailwindcss",
+        configurePostCss(postcssOptions) {
+          // Appends TailwindCSS and AutoPrefixer.
+          postcssOptions.plugins.push(require("tailwindcss"));
+          postcssOptions.plugins.push(require("autoprefixer"));
+          return postcssOptions;
+        },
+      };
+    },
+  ],
+
   presets: [
     [
       "classic",
